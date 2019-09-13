3a06c4405ec05b14fd5dcfab7944b2647c9d8f24
Euler Dias <eulerdias@ciandt.com>
2019-09-13 09:45:41 -0300

    Modelando arquitetura inicial e configuração inicial do store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,16 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { extractVuexModule } from 'vuex-class-component'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
+  modules: {
   }
 })
+
+const vxm = {
+  
+}
